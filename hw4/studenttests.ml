f59f409b0359d9fee93c37a371f5f7886460d160
remok <remok@student.ethz.ch>
2019-11-14 14:19:01 +0100

    added simple knapsack program as student provided testcase

--- hw4/studenttests.ml
+++ hw4/studenttests.ml
@@ -1,10 +1,15 @@
 open Assert
+open Gradedtests
 
 (* These tests are provided by you -- they will be graded manually *)
 
 (* You should also add additional test cases here to help you   *)
 (* debug your program.                                          *)
 
+let tests = [
+  ("knapsack.oat", "", "480")
+]
+
 let provided_tests : suite = [
-  
+  GradedTest ("knapsack", 5, executed_oat_file tests);
 ] 
