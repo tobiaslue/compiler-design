72e7df3d3b071052728cf370c524674e8939687b
Tobias <lutobias@student.ethz.ch>
2019-11-18 16:12:18 +0100

    erste versuech vo subtypes

--- hw05/typechecker.ml
+++ hw05/typechecker.ml
@@ -7,7 +7,7 @@ open Tctxt
 
 exception TypeError of string
 
-let type_error (l : 'a node) err = 
+let type_error (l : 'a node) err =
   let (_, (s, e), _) = l.loc in
   raise (TypeError (Printf.sprintf "[%d, %d] %s" s e err))
 
@@ -38,21 +38,46 @@ let typ_of_unop : Ast.unop -> Ast.ty * Ast.ty = function
   | Lognot       -> (TBool, TBool)
 
 (* subtyping ---------------------------------------------------------------- *)
-(* Decides whether H |- t1 <: t2 
+(* Decides whether H |- t1 <: t2
     - assumes that H contains the declarations of all the possible struct types
 
-    - you will want to introduce addition (possibly mutually recursive) 
+    - you will want to introduce addition (possibly mutually recursive)
       helper functions to implement the different judgments of the subtyping
       relation. We have included a template for subtype_ref to get you started.
       (Don't forget about OCaml's 'and' keyword.)
 *)
 let rec subtype (c : Tctxt.t) (t1 : Ast.ty) (t2 : Ast.ty) : bool =
-  failwith "todo: subtype"
+  begin match t1, t2 with
+    |TInt, TInt -> true
+    |TBool, TBool -> true
+    |TNullRef ref1, TNullRef ref2 -> subtype_ref c ref1 ref2
+    |TRef ref1, TRef ref2 -> subtype_ref c ref1 ref2
+    |TRef ref1, TNullRef ref2 -> subtype_ref c ref1 ref2
+    |_ -> false
+  end
 
 (* Decides whether H |-r ref1 <: ref2 *)
 and subtype_ref (c : Tctxt.t) (t1 : Ast.rty) (t2 : Ast.rty) : bool =
-  failwith "todo: subtype_ref"
+  let f = fun t1 t2 -> subtype c t2 t1 in
+  begin match t1, t2 with
+    |RString, RString -> true
+    |RArray a1, RArray a2 -> true
+    |RStruct s1, RStruct s2 ->
+      begin match lookup_struct_option s1 c, lookup_struct_option s2 c with
+        |Some x, Some y -> true
+        |_ -> false
+      emd
+    |RFun (args1, ret1), RFun (args2, ret2) ->
+      List.for_all (fun b -> b) @@ [subtype_ret c ret1 ret2] @ (List.map2 f args1 args2)
+    |_ -> false
+  end
 
+and subtype_ret (c : Tctxt.t) (t1 : Ast.ret_ty) (t2 : Ast.ret_ty) : bool =
+  begin match t1, t2 with
+    |RetVoid, RetVoid -> true
+    |RetVal ref1, RetVal ref2 -> subtype c ref1 ref2
+    |_ -> false
+  end
 
 (* well-formed types -------------------------------------------------------- *)
 (* Implement a (set of) functions that check that types are well formed according
@@ -61,8 +86,8 @@ and subtype_ref (c : Tctxt.t) (t1 : Ast.rty) (t2 : Ast.rty) : bool =
     - the function should succeed by returning () if the type is well-formed
       according to the rules
 
-    - the function should fail using the "type_error" helper function if the 
-      type is 
+    - the function should fail using the "type_error" helper function if the
+      type is
 
     - l is just an ast node that provides source location information for
       generating error messages (it's only needed for the type_error generation)
@@ -102,8 +127,8 @@ let rec typecheck_exp (c : Tctxt.t) (e : Ast.exp node) : Ast.ty =
 
 (* statements --------------------------------------------------------------- *)
 
-(* Typecheck a statement 
-   This function should implement the statment typechecking rules from oat.pdf.  
+(* Typecheck a statement
+   This function should implement the statment typechecking rules from oat.pdf.
 
    Inputs:
     - tc: the type context
@@ -115,22 +140,22 @@ let rec typecheck_exp (c : Tctxt.t) (e : Ast.exp node) : Ast.ty =
        after this statement
      - A boolean indicating the return behavior of a statement:
         false:  might not return
-        true: definitely returns 
+        true: definitely returns
 
         in the branching statements, both branches must definitely return
 
-        Intuitively: if one of the two branches of a conditional does not 
-        contain a return statement, then the entier conditional statement might 
+        Intuitively: if one of the two branches of a conditional does not
+        contain a return statement, then the entier conditional statement might
         not return.
-  
-        looping constructs never definitely return 
+
+        looping constructs never definitely return
 
    Uses the type_error function to indicate a (useful!) error message if the
    statement is not type correct.  The exact wording of the error message is
    not important, but the fact that the error is raised, is important.  (Our
    tests also do not check the location information associated with the error.)
 
-   - You will probably find it convenient to add a helper function that implements the 
+   - You will probably find it convenient to add a helper function that implements the
      block typecheck rules.
 *)
 let rec typecheck_stmt (tc : Tctxt.t) (s:Ast.stmt node) (to_ret:ret_ty) : Tctxt.t * bool =
@@ -138,7 +163,7 @@ let rec typecheck_stmt (tc : Tctxt.t) (s:Ast.stmt node) (to_ret:ret_ty) : Tctxt.
 
 
 (* struct type declarations ------------------------------------------------- *)
-(* Here is an example of how to implement the TYP_TDECLOK rule, which is 
+(* Here is an example of how to implement the TYP_TDECLOK rule, which is
    is needed elswhere in the type system.
  *)
 
@@ -150,12 +175,12 @@ let rec check_dups fs =
 
 let typecheck_tdecl (tc : Tctxt.t) id fs  (l : 'a Ast.node) : unit =
   if check_dups fs
-  then type_error l ("Repeated fields in " ^ id) 
+  then type_error l ("Repeated fields in " ^ id)
   else List.iter (fun f -> typecheck_ty l tc f.ftyp) fs
 
 (* function declarations ---------------------------------------------------- *)
-(* typecheck a function declaration 
-    - extends the local context with the types of the formal parameters to the 
+(* typecheck a function declaration
+    - extends the local context with the types of the formal parameters to the
       function
     - typechecks the body of the function (passing in the expected return type
     - checks that the function actually returns
@@ -184,7 +209,7 @@ let typecheck_fdecl (tc : Tctxt.t) (f : Ast.fdecl) (l : 'a Ast.node) : unit =
    create_global_ctxt: - typechecks the global initializers and adds
    their identifiers to the 'G' global context
 
-     H ; G1 |-g prog ==> G2    
+     H ; G1 |-g prog ==> G2
 
 
    NOTE: global initializers may mention function identifiers as
@@ -200,8 +225,8 @@ let create_global_ctxt (tc:Tctxt.t) (p:Ast.prog) : Tctxt.t =
   failwith "todo: create_function_ctxt"
 
 
-(* This function implements the |- prog and the H ; G |- prog 
-   rules of the oat.pdf specification.   
+(* This function implements the |- prog and the H ; G |- prog
+   rules of the oat.pdf specification.
 *)
 let typecheck_program (p:Ast.prog) : unit =
   let sc = create_struct_ctxt p in
@@ -210,5 +235,5 @@ let typecheck_program (p:Ast.prog) : unit =
   List.iter (fun p ->
     match p with
     | Gfdecl ({elt=f} as l) -> typecheck_fdecl tc f l
-    | Gtdecl ({elt=(id, fs)} as l) -> typecheck_tdecl tc id fs l 
+    | Gtdecl ({elt=(id, fs)} as l) -> typecheck_tdecl tc id fs l
     | _ -> ()) p
