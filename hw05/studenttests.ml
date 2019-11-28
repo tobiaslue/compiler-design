c1b793e50cc6f0b896470c30550f05dd6e3446d5
remok <remok@student.ethz.ch>
2019-11-28 15:00:46 +0100

    first testcases added

--- hw05/studenttests.ml
+++ hw05/studenttests.ml
@@ -1,10 +1,31 @@
 open Assert
+open Ast
 
 (* These tests are provided by you -- they will be graded manually *)
 
 (* You should also add additional test cases here to help you   *)
 (* debug your program.                                          *)
 
+let unit_tests = [
+  ("CArr expression",
+    (fun () -> 
+      try
+        if (Typechecker.typecheck_exp Tctxt.empty (no_loc (CArr (TInt, [(no_loc (CInt 8L))]))) = TRef (RArray TInt)) then ()
+        else failwith "typecheck_exp returned wrong type"
+      with Typechecker.TypeError _ -> failwith "should not fail"
+    )
+  );
+  ("no CArr expression",
+    (fun () ->
+      try
+        if (Typechecker.typecheck_exp Tctxt.empty (no_loc (CArr (TInt, [(no_loc (CBool true))]))) = TRef (RArray TInt)) then failwith "should fail"
+        else ();
+        failwith "should fail"
+      with Typechecker.TypeError _ -> ();
+    ) 
+  )
+]
+
 let provided_tests : suite = [
-  
+  GradedTest("Student provided unit tests", 5, unit_tests);  
 ] 
